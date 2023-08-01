bbaafbb9264f0ab23f26b4e300dbfebcd37ef14a
Amith Kini <kini.amit.22@gmail.com>
2023-07-31 19:59:48 -0500

    Add some cool animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 (function () {
+    nameHover();
     'use strict';
     setSectionToggleEventHandler();
     skillHover();
@@ -56,4 +57,35 @@ function skillHover() {
             container.style.setProperty('--fill-height', '0');
         });
     });
+}
+
+function nameHover() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let interval = null;
+
+    document.querySelector("h1").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+        event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+            if(index < iteration) {
+            return event.target.dataset.value[index];
+            }
+        
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+        
+        if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+        }
+        
+        iteration += 1 / 3;
+    }, 30);
+    }
 }
